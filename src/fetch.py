cf32471a153048984d1476fc921cdecfbc4dd42e
Philip Theus <philip@simplexity.quest>
2024-08-20 16:15:19 -0400

    WIP get historic data

--- src/fetch.py
+++ src/fetch.py
@@ -1,5 +1,6 @@
 import adafruit_requests
 import os
+from adafruit_datetime import datetime, timedelta
 
 SUPABASE_URL: str = os.getenv("SUPABASE_URL", "")
 if SUPABASE_URL == "":
@@ -12,22 +13,54 @@ def get_dashboard_data(requests: adafruit_requests.Session, zone: str):
     Compute display values from raw data.
     """
 
-    # get the latest row in the electricitymaps-hourly table for each zone (where testing is false)
+    print("Fetching dashboard data...")
+
+    # don't trust the system clock. Get the current time from the internet
+    response = requests.get("http://worldtimeapi.org/api/timezone/UTC")
+
+    if response.status_code != 200:
+        raise ValueError(f"Failed to fetch time data: {response.status_code}. Error: {response.text}")
+
+    time_data = response.json()
+    now_utc: datetime = datetime.fromtimestamp(time_data["unixtime"])
+
+    # Calculate the start and end dates for the last week
+    start_date = now_utc - timedelta(days=2)
+
+    # Get all rows within the last week for the specified zone
     response = requests.get(
         f"{SUPABASE_URL}/rest/v1/electricitymaps-hourly?" +
-            "select=carbon_intensity_raw,power_breakdown_raw&order=created_at.desc&limit=1" +
-            f"&testing=neq.True&zone=eq.{zone}",
+            "select=carbon_intensity_raw,power_breakdown_raw&order=created_at.asc" +
+            f"&created_at=gte.{start_date}&zone=eq.{zone}&testing=neq.True",
         headers={"apikey": SUPABASE_KEY},
     )
 
     if response.status_code != 200:
         raise ValueError(f"Failed to fetch electricitymaps data: {response.status_code}. Error: {response.json()}")
 
-    data = response.json()[0]
+    data = response.json()
 
-    carbon_intensity_history: list[int] = [row["carbonIntensity"] for row in data["carbon_intensity_raw"]["history"]]
+    # Filter out duplicate rows for each hour and build the 7-day history
+    carbon_intensity_history = []
+    power_consumption_history = []
 
-    power_consumption_history: list[int] = [row["powerConsumptionTotal"] for row in data["power_breakdown_raw"]["history"]]
+    for row in data:
+        carbon_intensity_history.append(row["carbon_intensity_raw"]["history"][-1])
+        power_consumption_history.append(row["power_breakdown_raw"]["history"][-1])
+
+    # add remaining 23 hours of the latest row
+    latest_row = data[-1]
+
+    for i in reversed(range(23)):
+        carbon_intensity_history.append(latest_row["carbon_intensity_raw"]["history"][i])
+        power_consumption_history.append(latest_row["power_breakdown_raw"]["history"][i])
+
+    print(len(carbon_intensity_history))
+    print([row["datetime"] for row in carbon_intensity_history])
+
+    # Extract numeric values (carbonIntensity, powerConsumptionTotal) from the raw data
+    carbon_intensity_history = [datapoint["carbonIntensity"] for datapoint in carbon_intensity_history]
+    power_consumption_history = [datapoint["powerConsumptionTotal"] for datapoint in power_consumption_history]
 
     # utility data for Philip
     response = requests.get(
